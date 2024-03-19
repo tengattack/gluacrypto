19d4eb32907b1b3387cccc352f755ff5a313a24b
Root-lee <irootlee@gmail.com>
2024-03-20 00:12:34 +0800

    feat: add url-safe base64 encode/decode func

--- crypto/base64.go
+++ crypto/base64.go
@@ -24,3 +24,22 @@ func base64DecodeFn(L *lua.LState) int {
 	L.Push(lua.LString(result))
 	return 1
 }
+
+func base64URLEncodeFn(L *lua.LState) int {
+	s := lua.LVAsString(L.Get(1))
+	result := base64.URLEncoding.EncodeToString([]byte(s))
+	L.Push(lua.LString(result))
+	return 1
+}
+
+func base64URLDecodeFn(L *lua.LState) int {
+	s := lua.LVAsString(L.Get(1))
+	result, err := base64.URLEncoding.DecodeString(s)
+	if err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+	L.Push(lua.LString(result))
+	return 1
+}
--- crypto/base64_test.go
+++ crypto/base64_test.go
@@ -50,3 +50,45 @@ func TestBase64Decode(t *testing.T) {
 	assert.Nil(err)
 	assert.Equal(string(Data), val)
 }
+
+func TestBase64URLEncode(t *testing.T) {
+	assert := assert.New(t)
+
+	// test start
+	L := lua.NewState()
+	defer L.Close()
+	gluacrypto.Preload(L)
+
+	script := `
+		crypto = require('crypto')
+ 		return crypto.base64_url_encode('` + string(Data) + `')
+	`
+	assert.NoError(L.DoString(script))
+
+	val := getValue(L, L.Get(1))
+	err := getValue(L, L.Get(2))
+	assert.Nil(err)
+	assert.Equal(base64.URLEncoding.EncodeToString(Data), val)
+}
+
+func TestBase64URLDecode(t *testing.T) {
+	assert := assert.New(t)
+
+	// test start
+	L := lua.NewState()
+	defer L.Close()
+	gluacrypto.Preload(L)
+
+	b64data := base64.URLEncoding.EncodeToString(Data)
+
+	script := `
+		crypto = require('crypto')
+ 		return crypto.base64_url_decode('` + b64data + `')
+	`
+	assert.NoError(L.DoString(script))
+
+	val := getValue(L, L.Get(1))
+	err := getValue(L, L.Get(2))
+	assert.Nil(err)
+	assert.Equal(string(Data), val)
+}
